1b65afa9d33ce7a2b4a80ce7c3f44fcf24add68e
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-02-27 12:29:48 -0800

    reroute homepage

--- app.js
+++ app.js
@@ -2,13 +2,8 @@ var newsBeat = angular.module('newsBeat', ['ui.router']);
 
 newsBeat.config(function($stateProvider) {
 
-  $stateProvider.state('home', {
-    url: "",
-    templateUrl: "partials/home.html"
-  });
-
   $stateProvider.state('beats', {
-    url: "/beats",
+    url: "",
     templateUrl: "partials/beats.html",
     controller: 'BeatsCtrl'
   });
