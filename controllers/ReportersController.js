97152141417443c64b5189be81fda6f8f1ae2a9a
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-02-27 12:49:40 -0800

    add ability to list out reporter information

--- controllers/ReportersController.js
+++ controllers/ReportersController.js
@@ -2,7 +2,7 @@ newsBeat.controller('ReportersCtrl', function ReportersCtrl($scope, $stateParams
   $scope.beat = UtilitiesFactory.findById(BeatsFactory.beats, $stateParams.beatId),
   $scope.addReporter = function() {
     $scope.beat.reporters = [];
-    $scope.beat.reporters.push({ name: $scope.beat.reporter });
+    $scope.beat.reporters.push({ name: $scope.beat.reporter, employment: $scope.beat.duration, birthday: $scope.beat.birthday });
     $scope.reporter = null;
   }
 });
