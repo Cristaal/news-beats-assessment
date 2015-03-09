e9ec0c97c15433971efdafcf871348c2ddd73486
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-03-09 10:12:53 -0700

    allow user to enter more than one reporter for a beat

--- controllers/ReportersController.js
+++ controllers/ReportersController.js
@@ -1,8 +1,9 @@
 newsBeat.controller('ReportersCtrl', function ReportersCtrl($scope, $stateParams, BeatsFactory, UtilitiesFactory) {
   $scope.beat = UtilitiesFactory.findById(BeatsFactory.beats, $stateParams.beatId),
   $scope.addReporter = function() {
-    $scope.beat.reporters = [];
     $scope.beat.reporters.push({ name: $scope.beat.reporter, employment: $scope.beat.duration, birthday: $scope.beat.birthday });
-    $scope.reporter = null;
+    $scope.name = null;
+    $scope.employment = null;
+    $scope.birthday = null;
   }
 });
