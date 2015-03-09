eba85740516c8e8f0be7d6456c89aabf91552e04
Crystal Contreras-Grossman <crystalcontrerasgrossman@gmail.com>
2015-03-09 10:43:46 -0700

    clear form field after submit

--- controllers/ReportersController.js
+++ controllers/ReportersController.js
@@ -2,8 +2,8 @@ newsBeat.controller('ReportersCtrl', function ReportersCtrl($scope, $stateParams
   $scope.beat = UtilitiesFactory.findById(BeatsFactory.beats, $stateParams.beatId),
   $scope.addReporter = function() {
     $scope.beat.reporters.push({ name: $scope.beat.reporter, employment: $scope.beat.duration, birthday: $scope.beat.birthday });
-    $scope.name = null;
-    $scope.employment = null;
-    $scope.birthday = null;
+    $scope.beat.reporter = null;
+    $scope.beat.duration = null;
+    $scope.beat.birthday = null;
   }
 });
